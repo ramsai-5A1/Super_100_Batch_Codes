130f46469916a234bb76f1d05f2f920f50087f83
Manapuram Ram sai <83913300+ramsai-5A1@users.noreply.github.com>
2024-06-13 15:47:32 +0530

    Update 9_sliding_window_maximum.py

--- 9_sliding_window_maximum.py
+++ 9_sliding_window_maximum.py
@@ -1,18 +1,38 @@
 class Solution(object):
     def maxSlidingWindow(self, nums, k):
-        result = []
-        dq = []
         n = len(nums)
-        for index in range(n):
+        result = []
+        store = []
+        
+        #[300, 20, 80, 90, 150]
+        # k = 4
+        # [0, 1, 2, ]
+        
+        for i in range(k):
+            # step-2 (eliminate all smaller elements indices)
+            while store and nums[store[-1]] <= nums[i]:
+                store.pop()
+    
+            # step-3 (insert current index)
+            store.append(i)
+        
+        result.append(nums[store[0]])
+        
+        
+        left, right = 0, k
+        while right < n:
+            # step-1 (eliminate unwanted indices)
+            if store and store[0] == left:
+                store.pop(0)
 
-            while len(dq) > 0 and dq[0][0] <= index - k:
-                dq.pop(0)
+            # step-2 (eliminate all smaller elements indices)
+            while store and nums[store[-1]] <= nums[right]:
+                store.pop()
 
-            while len(dq) > 0 and dq[-1][1] <= nums[index]:
-                dq.pop()
-
-            dq.append([index, nums[index]])
-            if index >= k - 1:
-                result.append(dq[0][1])
-
+            # step-3 (insert current index)
+            store.append(right)
+            result.append(nums[store[0]])
+            left += 1 
+            right += 1 
+            
         return result
