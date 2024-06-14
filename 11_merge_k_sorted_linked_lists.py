f4a7ad8baa4350f771336844b1bf9ff33cf7e1c2
Manapuram Ram sai <83913300+ramsai-5A1@users.noreply.github.com>
2024-06-14 14:32:49 +0530

    Create 11_merge_k_sorted_linked_lists.py

---
+++ 11_merge_k_sorted_linked_lists.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        n = len(lists)
+        if n == 0:
+            return None
+
+        def mergeTwoSortedLists(list1, list2):
+            if list1 == None:
+                return list2 
+            elif list2 == None:
+                return list1 
+            
+            dummyNode = ListNode(-1)
+            tail = dummyNode 
+            head = dummyNode 
+    
+            while list1 != None and list2 != None:
+                if list1.val < list2.val:
+                    tail.next = list1 
+                    list1 = list1.next 
+                else:
+                    tail.next = list2 
+                    list2 = list2.next 
+                tail = tail.next
+    
+            if list1:
+                tail.next = list1 
+            else:
+                tail.next = list2
+            return head.next
+
+
+
+
+        def helper(index):
+            if index == n - 1:
+                return lists[index] 
+
+            head2 = helper(index + 1)
+            head1 = lists[index]
+            return mergeTwoSortedLists(head1, head2)
+        return helper(0)
+        
