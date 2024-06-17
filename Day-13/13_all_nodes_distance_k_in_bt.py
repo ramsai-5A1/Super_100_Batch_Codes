4696497418c6418cdc65879c5f55f7e5dd2e36a8
Ramsai <ramsaicrony@gmail.com>
2024-06-17 19:30:25 +0530

    Add nodes at distance k in bt

---
+++ Day-13/13_all_nodes_distance_k_in_bt.py
@@ -0,0 +1,51 @@
+
+class Solution(object):
+    def distanceK(self, root, target, k):
+        result = []
+
+        def addAllKSeperated(currNode, distance):
+            if distance > k or not currNode:
+                return 
+            elif distance == k:
+                result.append(currNode.val)
+                return 
+            addAllKSeperated(currNode.left, distance + 1)
+            addAllKSeperated(currNode.right, distance + 1)
+
+
+        def helper(currNode):
+            if not currNode:
+                return [False, -1]
+            if currNode.val == target.val:
+                addAllKSeperated(currNode, 0)
+                return [True, 1]
+                
+            leftVal = helper(currNode.left)
+            if leftVal[0]:
+                if leftVal[1] == k:
+                    result.append(currNode.val)
+                addAllKSeperated(currNode.right, leftVal[1] + 1)
+                return [True, leftVal[1] + 1]
+
+            rightVal = helper(currNode.right)
+            if rightVal[0]:
+                if rightVal[1] == k:
+                    result.append(currNode.val)
+                addAllKSeperated(currNode.left, rightVal[1] + 1)
+                return [True, rightVal[1] + 1]
+
+            if (leftVal[0] and leftVal[1] == k) or (rightVal[0] and rightVal[1] == k):
+                result.append(currNode.val)
+
+            
+            if leftVal[0]:
+                leftVal[1] += 1 
+                return leftVal 
+            elif rightVal[0]:
+                rightVal[1] += 1 
+                return rightVal 
+            return [False, -1]
+
+
+        helper(root)
+        return result
